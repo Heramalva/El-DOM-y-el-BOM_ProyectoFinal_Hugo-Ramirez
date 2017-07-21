2d593ebc738591177244e9bd054a5533e5916a34
Heramalva <heramalva@gmail.com>
2017-07-20 21:32:49 -0600

    proyecto terminado
    se corrigo el problema javascript y se agregaron estilos css

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,80 @@
-var estudiantes = [];
-
+var myObj;
+var datos=[];
 
 //Funcion para registrar estudiantes
 	function registrar(){
-		// agrega los estudiantes a una tabla
-			var tabla=document.getElementById("tabla");
-			var row = tabla.insertRow();
-  		var cell1 = row.insertCell(0);
-  		var cell2 = row.insertCell(1);
-  		var cell3 = row.insertCell(2);
-  		var a=document.getElementById("campo1").value;
-  		var b=document.getElementById("campo2").value;
-  		var c=document.getElementById("campo3").value;
-  		cell1.innerHTML = a;
-  		cell2.innerHTML = b;
-  		cell3.innerHTML = c;
+		//verificando datos correctos y erroneros
+		if ((campo1.value==='')||(campo2.value==='')||(campo3.value==='')) {
+			alert("Debe Ingresar Datos");
+		}else if ((campo3.value<0)||(campo3.value>101)) {
+			alert("Debe Ingresar un Numero de 0 a 100");
+			campo3.value="";
+		}else{
+			//captura datos
+				var a=document.getElementById("campo1").value;
+				var b=document.getElementById("campo2").value;
+				var c=document.getElementById("campo3").value;
 
-		//agregar los estudiantes a un JSON
-  		estudiantes.codigo=a;
-  		estudiantes.nombre=b;
-  		estudiantes.nota=parseInt(c);
+			// agrega los estudiantes a la tabla
+				var tabla=document.getElementById("tabla");
+				var row = tabla.insertRow();
+	  		var cell1 = row.insertCell(0);
+	  		var cell2 = row.insertCell(1);
+	  		var cell3 = row.insertCell(2);
+	  		cell1.innerHTML = a;
+	  		cell2.innerHTML = b;
+	  		cell3.innerHTML = c;
 
-  		var datos=[];
-  		datos.push(estudiantes);
+			//agregar los estudiantes a un JSON
+				myObj = { "codigo":a, "nombre":b, "nota":parseInt(c) };
+	    	datos.push(myObj);
 
-		//comprobando en consola que guarda los datos en JSON
-  		console.log(datos);
+			//comprobando en consola que guarda los datos en JSON
+	  		console.log(datos);
 
-		//limpiar los campos para el proximo estudiante
-  		campo1.value="";
-  		campo2.value="";
-  		campo3.value="";
+			//limpiar los campos para el proximo estudiante
+	  		campo1.value="";
+	  		campo2.value="";
+	  		campo3.value="";
+			}
 		}
+
+
+		//Funcion para calcular el Promedio
+			function promedio(){
+				var b=0;
+				for(i=0;i<datos.length;i++){
+					b+=(datos[i].nota)/datos.length;
+				}
+				document.getElementById("texto1").innerHTML=b.toFixed(2);
+			}
+
+			// Funcion para encontrar el numero mayor y mostrar el nombre
+	function mayor(){
+		var mayor=-1;
+		var contador=0;
+		var c="";
+		while (contador<datos.length) {
+			if (datos[contador].nota>mayor) {
+				mayor=datos[contador].nota;
+				c=datos[contador].nombre;
+			}
+			contador++;
+		}
+		document.getElementById("texto2").innerHTML=c;
+	}
+
+//Funcion para encontrar el numero menor y mostrar el nombre
+	function menor(){
+		var menor=101;
+		var contador=0;
+		var d="";
+		while (contador<datos.length) {
+			if (datos[contador].nota<menor) {
+				menor=datos[contador].nota;
+				d=datos[contador].nombre;
+			}
+			contador++;
+		}
+		document.getElementById("texto3").innerHTML=d;
+	}
