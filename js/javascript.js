86023070d5d0b8e02652961b966807b5fb8212b4
Heramalva <heramalva@gmail.com>
2017-07-20 18:24:28 -0600

    intento3
    nada

--- js/javascript.js
+++ js/javascript.js
@@ -3,17 +3,9 @@ var estudiantes = [];
 
 //Funcion para registrar estudiantes
 	function registrar(){
-		if ((document.getElementById("campo1").value<0)||(document.getElementById("campo1").value>100)) {
-			alert("Debe Ingresar una Nota de 0 a 100");
-
-			if (document.getElementById("campo3").value>0) {
-				alert("Debe Ingresar un Nombre Valido");
-			}
-		}
-
-		else{
-		var tabla=document.getElementById("tabla");
-		var row = tabla.insertRow();
+		// agrega los estudiantes a una tabla
+			var tabla=document.getElementById("tabla");
+			var row = tabla.insertRow();
   		var cell1 = row.insertCell(0);
   		var cell2 = row.insertCell(1);
   		var cell3 = row.insertCell(2);
@@ -24,6 +16,7 @@ var estudiantes = [];
   		cell2.innerHTML = b;
   		cell3.innerHTML = c;
 
+		//agregar los estudiantes a un JSON
   		estudiantes.codigo=a;
   		estudiantes.nombre=b;
   		estudiantes.nota=parseInt(c);
@@ -31,50 +24,13 @@ var estudiantes = [];
   		var datos=[];
   		datos.push(estudiantes);
 
-  		console.log(datos);
+		//comprobando en consola que guarda los datos en JSON
+  		console.log(estudiantes);
+			var types=JSON.parse(estudiantes);
+			console.log(types);
 
+		//limpiar los campos para el proximo estudiante
   		campo1.value="";
   		campo2.value="";
   		campo3.value="";
 		}
-	}
-
-
-//Funcion para calcular el Promedio
-	function promedio(){
-		var b=0;
-		for(i=0;i<estudiantes.length;i++){
-			b+=(estudiantes[i].nota)/10;
-		}
-		document.getElementById("texto2").innerHTML=b.toFixed(2);
-	}
-
-// Funcion para encontrar el numero mayor y mostrar el nombre
-	function mayor(){
-		var mayor=-1;
-		var contador=0;
-		var c="";
-		while (contador<estudiantes.length) {
-			if (estudiantes[contador].nota>mayor) {
-				mayor=estudiantes[contador].nota;
-				c=estudiantes[contador].nombre;
-			}
-			contador++;
-		}
-		document.getElementById("texto3").innerHTML=c;
-	}
-
-//Funcion para encontrar el numero menor y mostrar el nombre
-	function menor(){
-		var menor=101;
-		var contador=0;
-		var d="";
-		while (contador<estudiantes.length) {
-			if (estudiantes[contador].nota<menor) {
-				menor=estudiantes[contador].nota;
-				d=estudiantes[contador].nombre;
-			}
-			contador++;
-		}
-		document.getElementById("texto4").innerHTML=d;
-	}
